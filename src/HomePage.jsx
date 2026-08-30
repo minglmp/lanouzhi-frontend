b4170082de6540b752341106972354f849ad7e5b
minglmp <mingdaichi@gmail.com>
2026-08-31 06:13:33 +0700

    Remove delete button from Home page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -259,11 +259,6 @@ const HomePage = () => {
 
           <div className="flex-1 max-w-4xl">
             <div className="relative w-full">
-              <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
-                <svg className="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-              </div>
               <input 
                 type="text" 
                 value={searchQuery}
